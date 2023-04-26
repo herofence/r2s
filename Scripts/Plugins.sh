2b400e74968b8f4c6a855013c94745a7d4b5fc50
herofence <herofence@qq.com>
2023-04-26 09:14:12 +0800

    https://github.com/herofence/rockchip_r2s.git

---
+++ Scripts/Plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Argon主题
+git clone --depth=1 --single-branch --branch "18.06" https://github.com/jerrykuku/luci-theme-argon.git
+#Argon主题设置
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#Design主题
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+#Design主题设置
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#第三方源
+git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package.git
+#istore
+#git clone --depth=1 --single-branch https://github.com/linkease/istore
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages
---
+++ Scripts/Settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#删除冲突主题
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
+#修改默认主机名
+sed -i "s/OpenWrt/$OWRT_NAME/g" ./package/base-files/files/bin/config_generate
+#修改默认IP地址
+sed -i "s/192.168.1.1/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
+#修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
+#修改默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/lean/autocore/files/ -type f -name "index.htm")
