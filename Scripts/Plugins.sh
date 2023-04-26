bdb3a9821e39346dddd1f4cf323e7a588683c6c8
herofence <113574064+herofence@users.noreply.github.com>
2023-04-26 12:08:35 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-
-#Argon主题
+#Argon Theme
 git clone --depth=1 --single-branch --branch "18.06" https://github.com/jerrykuku/luci-theme-argon.git
-#Argon主题设置
+#Argon Config
 git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
-#Design主题
+#Design Theme
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
-#Design主题设置
+#Design Config
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #第三方源
-git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package.git
+git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package
 #istore
-#git clone --depth=1 --single-branch https://github.com/linkease/istore
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages
+git clone --depth=1 --single-branch https://github.com/linkease/istore
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages
+#8812cu
+git clone --depth=1 --single-branch https://github.com/srdgame/rtl8812cu-20200609
