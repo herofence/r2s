9118a8484bb7bc21e1c95cfad25d25eec585c866
herofence <113574064+herofence@users.noreply.github.com>
2023-05-15 07:50:00 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -11,8 +11,8 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 #第三方源
 git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package
 #istore
-#git clone --depth=1 --single-branch https://github.com/linkease/istore
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages
+#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 #8812cu
 #git clone --depth=1 --single-branch https://github.com/srdgame/rtl8812cu-20200609
