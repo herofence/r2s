7e0f83b2b4fc5e1ceffc938708194e5e16b3cd32
herofence <113574064+herofence@users.noreply.github.com>
2023-05-15 12:06:12 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
+#Linkease
+git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Argon Theme
-git clone --depth=1 --single-branch --branch https://github.com/jerrykuku/luci-theme-argon.git
-#Argon Config
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 #Design Theme
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
-#Design Config
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
-#第三方源
-git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package
-#istore
-#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
-#8812cu
-#git clone --depth=1 --single-branch https://github.com/srdgame/rtl8812cu-20200609
+#Open Clash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
